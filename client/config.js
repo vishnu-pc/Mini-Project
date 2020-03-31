89b7a61aca61a3ceb0f695cffb3fd9b4218e84fa
vishnu-pc <vishnupc1999@gmail.com>
2020-03-31 17:55:30 +0530

    succesful changes

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
-export const TITLE = 'Project Name'
+export const TITLE = 'XY value Prediction'
 export const TIMEOUT = 3000
 
 // All project constatnts goes here
--- client/index.js
+++ client/index.js
@@ -1,19 +1,35 @@
+import * as tf from '@tensorflow/tfjs';
 import { TITLE, TIMEOUT } from './config'
 
 async function main() {
 	// This is the entrypoint for the client side code
 	// Your code goes here
 	document.title = TITLE
-	const descriptionEle = document.getElementById('description')
-	descriptionEle.innerText = `The spinner is just for show. It will go away in ${TIMEOUT / 1000} seconds`
+	// Define a machine learning model for linear regression
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-	// Lets make that spinner go away
-	setTimeout(() => {
-		const spinnerEle = document.getElementById('demo-spinner')
+    // Specify loss and optimizer for model
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-		spinnerEle.parentNode.removeChild(spinnerEle)
-		descriptionEle.innerText = 'And the spinner is gone!'
-	}, TIMEOUT)
+    // Prepare training data
+    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+	const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6,1]);
+	
+	// Train the model and set predict button to active
+	model.fit(xs, ys, {epochs: 500}).then(() => {
+
+    // Use model to predict values
+    document.getElementById('predictButton').disabled = false;
+	document.getElementById('predictButton').innerText = "Predict";
+
+	// Register click event handler for predict button
+	document.getElementById('predictButton').addEventListener('click', (el, ev) => {
+	const val = document.getElementById('inputValue').value;
+	const parsedVal = parseInt(val)
+    document.getElementById('output').innerText = model.predict(tf.tensor2d([parsedVal], [1,1]));
+});
+});
 }
 
 main()
