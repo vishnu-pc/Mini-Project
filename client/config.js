1d28c48e465669d3d8af5c7fb267bbb7a2417683
vishnu-pc <vishnupc1999@gmail.com>
2020-03-31 18:36:17 +0530

    Function.js added

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
-export const TITLE = 'XY value Prediction'
+export const TITLE = 'Handwritten-Digit Prediction'
 export const TIMEOUT = 3000
 
 // All project constatnts goes here
--- client/index.js
+++ client/index.js
@@ -1,35 +1,11 @@
-import * as tf from '@tensorflow/tfjs';
+import handwritten_model from './function'
 import { TITLE, TIMEOUT } from './config'
 
 async function main() {
 	// This is the entrypoint for the client side code
 	// Your code goes here
 	document.title = TITLE
-	// Define a machine learning model for linear regression
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-    // Specify loss and optimizer for model
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-    // Prepare training data
-    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-	const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6,1]);
-	
-	// Train the model and set predict button to active
-	model.fit(xs, ys, {epochs: 500}).then(() => {
-
-    // Use model to predict values
-    document.getElementById('predictButton').disabled = false;
-	document.getElementById('predictButton').innerText = "Predict";
-
-	// Register click event handler for predict button
-	document.getElementById('predictButton').addEventListener('click', (el, ev) => {
-	const val = document.getElementById('inputValue').value;
-	const parsedVal = parseInt(val)
-    document.getElementById('output').innerText = model.predict(tf.tensor2d([parsedVal], [1,1]));
-});
-});
+	handwritten_model()
 }
 
 main()
