02e943a958b15e1daa0d43aacd4a687e4a14e228
vishnu-pc <vishnupc1999@gmail.com>
2020-04-10 00:56:32 +0530

    Mark-1

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@
  * This is the Main entry point for the server app.
  */
 
+require('@tensorflow/tfjs-node');
+
 const init = async () => {
 	// This function is called on server start
 	// Your code goes here...
 	console.log('Server initialization function called')
-	const tf = require('@tensorflow/tfjs');
 	//require('@tensorflow/tfjs-node');
 
   
